62c01a2bfbd5b251aec55bb939883b4ab5acd258
Ryan Wiener <ryanl.wiener@yahoo.com>
2017-08-18 08:18:18 -0700

    Trying to make a killer Landing page

---
+++ server.js
@@ -0,0 +1,36 @@
+var http = require('http');
+var url = require('url');
+var fs= require('fs');
+
+var server = http.createServer(function (request, response) {
+	var path = url.parse(request.url).pathname;
+	switch (path) {
+		case '/':
+			fs.readFile(__dirname + "index.html", function(error, data){
+				if (error) {
+					response.writeHead(404);
+					response.write("Sorry this page does not exist")
+					response.end();
+				} else {
+					response.writeHead(200, {"Content-Type": "text/html"});
+					response.write(data, "utf8");
+					response.end();
+				}
+			}
+		default:
+			fs.readFile(__dirname + path, function(error, data){
+				if (error) {
+					response.writeHead(404);
+					response.write("Sorry this page does not exist")
+					response.end();
+				} else {
+					response.writeHead(200, {"Content-Type": "text/html"});
+					response.write(data, "utf8");
+					response.end();
+				}
+			}
+		break;
+	}
+})
+
+server.listen(80, 'localhost');
