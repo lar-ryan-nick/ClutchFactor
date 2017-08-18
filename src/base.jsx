f9b8f1a55af1e5b883b0fd8b970c435c06d70d7a
Ryan Wiener <ryanl.wiener@yahoo.com>
2017-08-18 15:09:03 -0700

    Trying to make a killer Landing page

---
+++ src/base.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {};
+		this.goToMain = this.goToMain.bind(this);
+	}
+
+	goToMain() {
+		window.location = "https://clutchfactor.herokuapp.com/index.html";
+	}
+
+	render() {
+		return (
+			<div class="header">
+				<img class="headerLogo" src="images/ClutchFactor.png" onClick={this.goToMain}/>
+				<a class="headerMerchandise" href="https://clutchfactor.herokuapp.com/merchandise.html">Merchandise</a>
+				<a class="headerAccount" href="https://clutchfactor.herokuapp.com/account.html">Log In/Create an Account</a>
+			</div>
+		)
+	}
+}
+
+export default Header;
---
+++ src/index.jsx
@@ -0,0 +1,4 @@
+import ReactDom from 'react-dom';
+import Header from './base.jsx';
+
+ReactDom.render(<Header/>, document.getElementById("header"));
