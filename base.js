433997668c2a750814b71f8e87a0d70a9cb1437c
lar-ryan-nick <ryanl.wiener@yahoo.com>
2017-08-22 19:00:27 -0700

    Setting up Accounts

--- base.js
+++ base.js
@@ -1,7 +1,9 @@
-window.onresize = function() {
-	if (window.innerWidth / (window.innerHeight - 251) > 8 / 5) {
+function fixMainMinHeight() {
+	if (window.innerHeight - 251 <= 0 || window.innerWidth / (window.innerHeight - 251) > 8 / 5) {
 		document.getElementById("main").style.minHeight = window.innerWidth * 5 / 8;
 	} else {
 		document.getElementById("main").style.minHeight = "calc(100% - 251px)";
 	}
 }
+window.onresize = fixMainMinHeight;
+fixMainMinHeight();
