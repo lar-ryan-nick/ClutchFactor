32becfc10b86e1a6f72973c72592c820ecaeda7d
Ryan Wiener <ryanl.wiener@yahoo.com>
2017-08-25 14:32:34 -0700

    Clean up accounts and set up merchandise

--- src/merchandise.jsx
+++ src/merchandise.jsx
@@ -36,16 +36,26 @@ class MerchandiseItem extends React.Component {
 				<div className={merchandiseDivClass} onMouseOver={this.handleOnMouseOver} onMouseLeave={this.handleOnMouseExit}></div>
 			);
 		}
+		let colorText = " colors";
+		if (this.props.data.colors.length == 1) {
+			colorText = " color";
+		}
 		let merchandiseDivClass = "merchandiseDiv";
 		let merchandiseTitleClass = "merchandiseTitle";
+		let numColorsTitleClass = "numColorsTitle";
+		let priceTitleClass = "priceTitle";
 		if (this.state.hovered == true) {
 			merchandiseDivClass = "merchandiseDivHovered";
 			merchandiseTitleClass = "merchandiseTitleHovered";
+			numColorsTitleClass = "numColorsTitleHovered";
+			priceTitleClass = "priceTitleHovered";
 		}
 		return (
 			<div className={merchandiseDivClass} onMouseOver={this.handleOnMouseOver} onMouseLeave={this.handleOnMouseExit} onClick={this.handleOnClick}>
 				<img className="merchandiseImage" src={"images/" + this.props.data.modelname + this.props.data.articletype + this.props.data.colors[0] + ".png"}/>
 				<p className={merchandiseTitleClass}>{this.props.data.modelname + " " + this.props.data.articletype}</p>
+				<p className={numColorsTitleClass}>{this.props.data.colors.length + colorText}</p>
+				<p className={priceTitleClass}>{"$" + this.props.data.price}</p>
 			</div>
 		);
 	}
