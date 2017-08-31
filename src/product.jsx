fc2acfce2eae7da698f5099ae01d2c57b210d147
Ryan Wiener <ryanl.wiener@yahoo.com>
2017-08-31 08:13:54 -0700

    Doing some clean up before setting up purchasing

--- src/product.jsx
+++ src/product.jsx
@@ -28,18 +28,24 @@ class ProductInfo extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			response: ""
+			response: "",
+			addingToCart: false
 		};
 		this.addToCart = this.addToCart.bind(this);
 	}
 
 	addToCart() {
+		let newState = this.state;
+		newState.loading = true;
+		newState.response = "";
+		this.setState(newState);
 		let xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function() {
 			if (xhttp.readyState == 4 && xhttp.status == 200) {
 				console.log(xhttp.responseText);
 				let newState = this.state;
 				newState.response = xhttp.responseText;
+				newState.loading = false;
 				this.setState(newState);
 			}
 		}.bind(this);
@@ -69,13 +75,19 @@ class ProductInfo extends React.Component {
 			cartResponse.push(<img key="1" className="icon" src="images/RedXIcon.png"/>);
 			cartResponse.push(<p key="2" className="errorText">{this.state.response}</p>);
 		}
+		let bottom = [];
+		if (this.state.loading) {
+			bottom.push(<div className="loader"></div>);
+		} else {
+			bottom.push(<button className="addToCartButton" onClick={this.addToCart}>Add to Cart</button>);
+		}
 		return (
 			<div className="productInfoDiv">
 				<p className="productInfoTitle">{this.props.data.modelname + " " + this.props.data.articletype + " - " + this.props.data.color}</p>
 				<p className="numColorsTitle">{this.props.data.colors.length + colorText}</p>
 				{colors}
 				<p className="priceTitle">{"Price: $" + this.props.data.price}</p>
-				<button className="addToCartButton" onClick={this.addToCart}>Add to Cart</button>
+				{bottom}
 				<div className="responseDiv">
 					{cartResponse}
 				</div>
