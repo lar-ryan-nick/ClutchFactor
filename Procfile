61df72881e0df2564b9ea4198d42044afeebd763
Ryan Wiener <ryanl.wiener@yahoo.com>
2017-09-08 16:17:40 -0700

    Reorganized

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node server.js
+web: ./node_modules/.bin/webpack -p & node server.js
--- scripts/pushRemotes.sh
+++ scripts/pushRemotes.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-./node_modules/.bin/webpack -p;
 git add .;
 git commit -am "Reorganized";
 git push heroku master;
