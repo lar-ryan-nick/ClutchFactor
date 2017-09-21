88685118de9bf033cd0972d6a9875a0875c94f1d
Ryan Wiener <ryanl.wiener@yahoo.com>
2017-09-21 14:23:57 -0700

    Fixing a few bugs

--- client/jsx/cartDisplay.jsx
+++ client/jsx/cartDisplay.jsx
@@ -6,7 +6,7 @@ class CartDisplay extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			containerWidth: 0
+//			containerWidth: 0
 		};
 		this.handleRemove = this.handleRemove.bind(this);
 	}
@@ -14,19 +14,19 @@ class CartDisplay extends React.Component {
 	handleRemove(index) {
 		this.props.removeItem(index);
 	}
-
+/*
 	componentDidMount() {
 		let newState = this.state;
 		newState.containerWidth = this.containingDiv.offsetWidth;
 		this.setState(newState);
 	}
-
+*/
 	render() {
 		let cartItems = [];
 		let total = 0;
 		for (let i = 0; i < this.props.data.length; ++i) {
 			if (this.props.data[i] != null) {
-				cartItems.push(<CartItem key={i} data={this.props.data[i]} removeItem={this.handleRemove.bind(this, i)} fontSize={.045 * this.state.containerWidth}/>);
+				cartItems.push(<CartItem key={i} data={this.props.data[i]} removeItem={this.handleRemove.bind(this, i)}/>);
 				total += this.props.data[i].price;
 			}
 		}
@@ -35,16 +35,16 @@ class CartDisplay extends React.Component {
 		if (this.props.numCartItems != null) {
 			let text = " items";
 			if (this.props.numCartItems > 0) {
-				totalLabel = <p className="totalTitle" style={{fontSize: .06 * this.state.containerWidth}}>{"Total: $" + total}</p>;
+				totalLabel = <p className="totalTitle">{"Total: $" + total}</p>;
 				if (this.props.numCartItems == 1) {
 					text = " item";
 				}
 			}
-			top.push(<p key="1" className="cartTitle" style={{fontSize: .075 * this.state.containerWidth}}>{"You have " + this.props.numCartItems + text + " in your cart"}</p>);
+			top.push(<p key="1" className="cartTitle">{"You have " + this.props.numCartItems + text + " in your cart"}</p>);
 		}
 		let button = null;
 		if (this.props.button == true) {
-			button = <button className="CheckoutButton" style={{fontSize: .06 * this.state.containerWidth}} onClick={function() {window.location = "/checkout.html";}}>Click here to finish your order</button>;
+			button = <button className="CheckoutButton" onClick={function() {window.location = "/checkout.html";}}>Click here to finish your order</button>;
 		}
 		return (
 			<div ref={(input) => {this.containingDiv = input;}} className="cartDiv">
